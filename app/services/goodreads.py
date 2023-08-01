2b231be47057caeaf07c09f04bcabc652392bb7a
Viljami Kilkkilä <viljami.kilkkila@gmail.com>
2023-08-01 21:47:35 +0300

    Books now in columns with some effects

--- app/services/goodreads.py
+++ app/services/goodreads.py
@@ -30,12 +30,12 @@ class GoodReadsBooks:
 
     timestamp = time.time()
 
-    async def fetch_books(self) -> List[dict]:
+    async def fetch_books(self):
         """Fetch books from Goodreads
         # ? Note that this only returns latest 30 added books
         """
         if not self.fetch_interval and saved_books.exists():
-            return self.load_books
+            return self.load_books_in_ball
         my_books = []
         async with ClientSession() as client:
             async with client.get(URL) as request:
@@ -69,7 +69,7 @@ class GoodReadsBooks:
                     }
                     my_books.append(new_book)
         self.save_books(my_books)
-        return my_books
+        return self.load_books_in_ball
 
     @property
     def fetch_interval(self) -> bool:
@@ -88,3 +88,10 @@ class GoodReadsBooks:
         with open(saved_books, "r", encoding="UTF-8") as file:
             list_of_books = json.load(file)
         return list_of_books
+
+    @property
+    def load_books_in_ball(self) -> List[List[dict]]:
+        """Load repositories from file"""
+        with open(saved_books, "r", encoding="UTF-8") as file:
+            list_of_books = json.load(file)
+        return [list_of_books[i : i + 3] for i in range(0, len(list_of_books), 3)]
